ccc44bf32222e8d764cb6acfca315f48f33e4a9f
Aresky Berkane <areskyberkane@gmail.com>
2019-11-05 20:14:44 +0000

    finished

---
+++ controler.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/contrib/all' if development?
+
+require_relative './models/film.rb'
+
+also_reload('/models/*')
+
+get '/films' do
+@films = Film.all
+erb(:index)
+end
+
+get '/films/:num' do
+
+  @films = Film.all
+  @num = [:num].to_i
+  erb(:anchor_tags)
+end
